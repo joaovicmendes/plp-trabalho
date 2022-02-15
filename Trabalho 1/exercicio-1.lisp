544661e552c3720f39487cfd18eac1cd8d845e66
João Victor Mendes <mendesjoaovic@gmail.com>
2022-02-15 11:01:47 -0300

    Adicionando trabalho 1 ex 2

--- Trabalho 1/exercicio-1.lisp
+++ Trabalho 1/exercicio-1.lisp
@@ -9,4 +9,44 @@
 ;; - Por exemplo, dada a lista L = (a b z x 4.6 (a x) () (5 z x) ())
 ;; - Deve ser construída a lista ((a 2) (b 1) (z 2) (x 3) (4.6 1) (() 2) (5 1) )
 
+;; Função auxiliar que traz elementos de listas aninhadas para o primeiro nível
+;; ex: (squash '(A B (C (D)) E (F))) -> (A B C D E F)
+(defun squash (list) 
+    (cond   
+        ((null list) nil)
+        ((atom list) (list list))
+        (t (append (squash (car list)) (squash (cdr list))))
+    )
+)
 
+;; Função auxiliar que atualiza a frência de um elemento em uma lista de frequências
+;; ex: (update-frequency 'B '((A 1) (B 2) (C 3))) -> ((A 1) (B 3) (C 3))
+(defun update-frequency (element freq-list) 
+    (cond   
+        ((null freq-list) (list (list element 1))) ;; se a lista é vazia, elemento ainda não existe, então adicione com frequência 1
+        ((eq element (caar freq-list)) ;; se o primeiro elemento é o que queremos atualizar, incrementa sua frequência
+            (let*
+                ((frequency (cadr (car freq-list))))
+                (cons (list element (+ frequency 1)) (cdr freq-list)))) 
+        (t (cons (car freq-list) (update-frequency element (cdr freq-list)))) ;; se não é o primeiro, procura no resto da lista
+    )
+)
+
+;; Função que realiza o cálculo da frequência. Atualiza a frequência do elemento atual e calcula da cauda recursivamente.
+;; Essa ordem na operação (atualiza com o atual para depois chamar recursivo) é necessária para manter a ordem dos elementos.
+;; Alternativamente, deveriamos retornar a lista final invertida
+;; ex: (count-element-frequency-internal '(A A B C C D) '()) -> ((A 2) (B 1) (C 2) (D 1))
+(defun count-element-frequency-internal (list freq-list)
+    (cond
+        ((null list) freq-list)
+        (t (let*
+            ((updated-list (update-frequency (car list) freq-list )))
+            (count-element-frequency-internal (cdr list) updated-list)
+        ))
+    )
+)
+
+;; Função que encapsula funções anteriores
+;; ex: (count-element-frequency '(a b (a b c) d ((e d ) f))) -> ((A 2) (B 2) (C 1) (D 2) (E 1) (F 1))
+(defun count-element-frequency (list)
+    (count-element-frequency-internal (squash list) '()))
--- Trabalho 1/exercicio-3.lisp
+++ Trabalho 1/exercicio-3.lisp
@@ -4,21 +4,21 @@
 ;; filhos B e C da esquerda e direita respectivamente e estes não possuem descendentes.
 ;; Faca um programa em lisp que, dada uma árvore binária representada por uma lista,
 ;; retorne uma lista com o percurso natural.
-;; - (percurso_natural '(A (B ( ) C ( ) ) ) ) 
+;; - (percurso-natural '(A (B ( ) C ( ) ) ) ) 
 ;; - (B A C)
 
-(defun percurso_natural (root) 
+(defun percurso-natural (root) 
     (cond
         ((null root) nil)
         (t (let*
                 (
-                    (curr_node (car root)) ;; salva o elemento raíz
-                    (left_tree (percurso_natural (cadr root))) ;; visita subárvore esquerda
-                    (right_tree (percurso_natural (cdr (cdr (cadr root))))) ;; visita subárvore direita
+                    (curr-node (car root)) ;; salva o elemento raíz
+                    (left-tree (percurso-natural (cadr root))) ;; visita subárvore esquerda
+                    (right-tree (percurso-natural (cdr (cdr (cadr root))))) ;; visita subárvore direita
                 )
-                (append left_tree (cons curr_node right_tree)))))) ;; retorna (subárvore_esq raíz subárvore_dir)
+                (append left-tree (cons curr-node right-tree)))))) ;; retorna (subárvore-esq raíz subárvore-dir)
 
 ;; Exemplos
-(percurso_natural '(A (B () C ()))) ;; esperado: (B A C)
-(percurso_natural '(A (B (E () F ()) C (G ())))) ;; esperado: (E B F A G C)
-(percurso_natural '(A (B (E () F ()) C (() () G ())))) ;; esperado: (E B F A NIL C G)
+(percurso-natural '(A (B () C ()))) ;; esperado: (B A C)
+(percurso-natural '(A (B (E () F ()) C (G ())))) ;; esperado: (E B F A G C)
+(percurso-natural '(A (B (E () F ()) C (() () G ())))) ;; esperado: (E B F A NIL C G)
